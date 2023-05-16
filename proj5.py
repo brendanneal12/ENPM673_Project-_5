854c08dc85a8a70228fb9280c15761492771cb5d
jkim603 <113157667+jkim603@users.noreply.github.com>
2023-05-15 22:10:51 -0400

    homography for frames

---
+++ proj5.py
@@ -0,0 +1,80 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+
+
+
+def transformation(image):   
+    lower_orange = np.array([0,80,150]) #bgr thresholds
+    upper_orange = np.array([80,150,255])
+    # frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    col,row,_ = np.shape(image)
+    x1, y1, x2, y2 = 1130, 0, 1500, 300
+    mask = np.zeros((col, row), dtype=np.uint8)
+    rect_mask = cv.rectangle(mask, (x1, y1), (x2, y2), 255, -1)
+    image_rect = cv.bitwise_and(image, image, mask=rect_mask)
+    image_filt = cv.GaussianBlur(image_rect,(7,7),0)
+    hull_mask = cv.inRange(image_filt, lower_orange, upper_orange)
+    hull_filter = cv.bitwise_and(image_filt, image_filt, mask= hull_mask)
+    hull_edges = cv.Canny(hull_filter,50,200)
+    hull_lines = cv.HoughLinesP(hull_edges,rho=1,theta=np.pi/180,threshold=60,minLineLength=0,maxLineGap=50)
+    print(hull_lines)
+    # cv.imshow('Original',frame)
+    # cv.imshow('Gray', frame_gray)
+    # cv.imshow('Rectangle ROI',image_rect)
+    # cv.imshow('Edges',hull_edges)
+
+    ## commented out code can be used to visualize lines on image
+    # lines_out = frame.copy()
+    # for i in range(len(hull_lines)):
+    #     startPoint = (hull_lines[i][0][0], hull_lines[i][0][1])
+    #     endPoint = (hull_lines[i][0][2], hull_lines[i][0][3])
+    #     cv.line(lines_out, startPoint, endPoint, (255, 0, 0))
+    # cv.imshow('Line Output', lines_out)
+
+
+    p1 = [hull_lines[3][0][0], hull_lines[3][0][1]]
+    p2 = [hull_lines[3][0][2], hull_lines[3][0][3]]
+    p3 = [hull_lines[4][0][0], hull_lines[4][0][1]]
+    p4 = [hull_lines[4][0][2], hull_lines[4][0][3]]
+
+    # y_av = int(np.round(np.average((p1[1], p2[1], p3[1], p4[1]))))
+    # # print(y_av)
+
+    l1 = [p1[0], 190]
+    l2 = [p2[0], 190]
+    l3 = [p3[0]-3, 145+1]
+    l4 = [p4[0], 145]
+
+    # src_pts = []
+    # dst_pts = []
+    # for i in range(len(hull_lines)):
+    #     src_pts.append([hull_lines[i][0][0], hull_lines[i][0][1]])
+    #     src_pts.append([hull_lines[i][0][2], hull_lines[i][0][3]])
+    #     dst_pts.append([hull_lines[i][0][0], 173])
+    #     dst_pts.append([hull_lines[i][0][2], 173])
+
+    src_pts = np.array([p1,p2,p3,p4])
+    dst_pts = np.array([l1,l2,l3,l4])
+    print(src_pts)
+    print(dst_pts)
+
+    H, _ = cv.findHomography(src_pts, dst_pts)
+    print("Homography Matrix:",H)
+    return H
+
+
+ship_vid = cv.VideoCapture("hull.mp4")
+
+# while ship_vid.isOpened():
+ret, frame = ship_vid.read()
+if ret == True:
+    col, row, _= np.shape(frame)
+    H = transformation(frame)
+
+scale = 1
+img_out = cv.warpPerspective(frame, H, (scale*row, scale*col))
+
+cv.imshow('Transformed Image', img_out)
+cv.waitKey()
